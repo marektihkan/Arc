898f9a9f791523c2f191bc04ad4afd41a0f35514
Developer <info@saiku.ee>
2010-09-01 17:42:59 +0300

    Project updates: EnterpriseLibrary, Log4Net, MVC

--- Arc/Source/Arc.Infrastructure.Presentation.Mvc/ControllerFactory.cs
+++ Arc/Source/Arc.Infrastructure.Presentation.Mvc/ControllerFactory.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Arc.Infrastructure.Dependencies;
 
 namespace Arc.Infrastructure.Presentation.Mvc
 {
     public class ControllerFactory : DefaultControllerFactory
-    { 
-        protected override IController GetControllerInstance(Type controllerType)
+    {
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
-            return (IController) ServiceLocator.Resolve(controllerType);
+            return (IController)ServiceLocator.Resolve(controllerType);
+        }
+        
+        public override void ReleaseController(IController controller)
+        {
+            ServiceLocator.Release(controller);
         }
     }
 }

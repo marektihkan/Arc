a3be96c0cc2132aeea4d5513418e90782619dbc5
siimv <siim.viikman@uptime.ee>
2012-01-19 16:49:09 +0200

    Fixed rake tasks to comply with newer albacore

--- Arc/tasks/tests.rb
+++ Arc/tasks/tests.rb
@@ -6,15 +6,15 @@ task :tests => ['tests:unit', 'tests:integration']
 namespace :tests do   
   desc "Runs all unittests"
   nunit :unit do |test_runner|
-    test_runner.path_to_command = CONFIG[:paths][:nunit]
-    test_runner.assemblies = "#{CONFIG[:directories][:build]}/#{CONFIG[:directories][:tests]}/#{CONFIG[:solution]}.Unit.Tests.dll"
+    test_runner.command = CONFIG[:paths][:nunit]
+    test_runner.assemblies = FileList["#{CONFIG[:directories][:build]}/#{CONFIG[:directories][:tests]}/#{CONFIG[:solution]}.Unit.Tests.dll"]
     test_runner.options "/xml=#{CONFIG[:directories][:build]}/results/Unit.Tests.xml"
   end
 
   desc "Runs all integration tests"
   nunit :integration do |test_runner|
-    test_runner.path_to_command = CONFIG[:paths][:nunit]
-    test_runner.assemblies = "#{CONFIG[:directories][:build]}/#{CONFIG[:directories][:tests]}/#{CONFIG[:solution]}.Integration.Tests.dll"
+    test_runner.command = CONFIG[:paths][:nunit]
+    test_runner.assemblies = FileList["#{CONFIG[:directories][:build]}/#{CONFIG[:directories][:tests]}/#{CONFIG[:solution]}.Integration.Tests.dll"]
     test_runner.options "/xml=#{CONFIG[:directories][:build]}/results/Integration.Tests.xml"
   end
 end

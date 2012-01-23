2c78b6607d3cc017a3332d68924cee32d9031970
siimv <siim.viikman@uptime.ee>
2012-01-23 19:33:40 +0200

    Added non-generic mapper extension method for collections

--- Arc/src/Arc.Infrastructure/Mapping/MapperExtensions.cs
+++ Arc/src/Arc.Infrastructure/Mapping/MapperExtensions.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Arc.Infrastructure.Mapping
 {
@@ -27,6 +30,15 @@ namespace Arc.Infrastructure.Mapping
 		public static TDestination As<TDestination>(this object source, TDestination destination)
 		{
 			return Map.From(source, source.GetType()).To(destination);
+		}
+
+		public static IEnumerable<TDestination> As<TDestination>(this IEnumerable list)
+		{
+			var objType = (from object obj in list select obj.GetType()).FirstOrDefault();
+
+			return objType == null
+			       	? Enumerable.Empty<TDestination>()
+			       	: Map.CollectionOf(list, objType).To<TDestination>();
 		}
 	}
 }
